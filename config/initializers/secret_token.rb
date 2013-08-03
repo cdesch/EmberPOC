edc06c2b5a40c0b891ea3a3976c481f5159265f3
Chris <cdesch@gmail.com>
2013-08-03 15:19:02 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EmberPOC::Application.config.secret_token = 'd35ec5849cd8d077f89d065c705affdb37cef3bd25b79b82d6e3d2b4e541935c39ee4e8c972437d02ed9ba09b5d10aa7b099ce6457e1b9db3cc2493d0e203da1'
